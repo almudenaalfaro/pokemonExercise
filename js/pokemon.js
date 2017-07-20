b1a4dbdae2743d54add372fdaee8ed2e3376b41b
jclopmel <jclm1988@gmail.com>
2017-07-20 18:37:48 +0200

    Got every pokemon by type+div

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,30 +1,8 @@
-$.get('http://pokeapi.co/api/v2/pokemon/1/', pokedexData);
-$.get('http://pokeapi.co/api/v2/characteristic/1/', pokedexDescription);
-
-
-function pokedexData(data){
-
-	var tipoPokemon 	 = data;
-	var name			 = tipoPokemon.forms[0].name;
-	var type1 	 		 = tipoPokemon.types[0].type.name;
-	var type2 	 		 = tipoPokemon.types[1].type.name;
-	var pokeImg			 = tipoPokemon.sprites.front_default;
-
-	$("h2").append(name);
-	$(".type").append(type1+" / "+type2);
-	$("h2").before("<img class='card-img-top' src='" + pokeImg + "'>");
-}
-
-function pokedexDescription (data){
-	console.log("Funciona tambien!");
-	var descript 		 = data.descriptions[1].description;
-
-	$(".description").append(descript);	
-}
 
 $("select").change(choose);
 
 function choose(){
+
 	$.get('http://pokeapi.co/api/v2/type/', selectType);
 	function selectType(data){
 		var dataType     = data.results;
@@ -34,11 +12,34 @@ function choose(){
 				function findType(data){													//Devuelve los datos del tipo seleccionado
 
 					var typeUrl = data.pokemon;
-					for (var i=0; i< 10; i++)												//typeUrl.length para sacarlos todos
+					for (var i=0; i< 1; i++){												//typeUrl.length para sacarlos todos
 
-					$.get(typeUrl[i].pokemon.url, pokedexData);								//Se llama a cada una de las url y se ejecuta la funcion
+						$.get(typeUrl[i].pokemon.url, pokedexData);							//Se llama a cada una de las url y se ejecuta la funcion
+						//$.get(typeUrl[i].pokemon.url, pokedexDescription);
+					}	
 
+					function pokedexData(data){
 
+						var tipoPokemon 	 = data;
+						var name			 = tipoPokemon.forms[0].name;
+						var type1 	 		 = tipoPokemon.types[0].type.name;
+				//		var type2 	 		 = tipoPokemon.types[1].type.name;
+						var pokeImg			 = tipoPokemon.sprites.front_default;
+
+						$(".container-fluid").append("<div class='container-fluid card2'></div>");
+						$(".card2").prepend("<p>Type: "+type1+"</p>");
+						$(".card2").prepend("<h4>Name: "+name+"</h4>");
+						
+						$(".card2").prepend("<img class='card-img-top' src='" + pokeImg + "'>");
+					}
+
+				/*	function pokedexDescription (data){
+						console.log("Funciona tambien!");
+						var descript 		 = data.descriptions[1].description;
+
+						$(".description").append(descript);	
+					}
+				*/
 				}
 
 				
