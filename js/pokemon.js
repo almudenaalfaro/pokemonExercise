c7be40377a8e099c0e46583000c5b8d20dfca86e
jclopmel <jclm1988@gmail.com>
2017-07-20 14:18:44 +0200

    first card completed+jQuery

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,5 @@
 $.get('http://pokeapi.co/api/v2/pokemon/1/', pokedexData);
+$.get('http://pokeapi.co/api/v2/characteristic/1/', pokedexDescription);
 
 
 /*$.ajax({
@@ -10,7 +11,19 @@ $.get('http://pokeapi.co/api/v2/pokemon/1/', pokedexData);
 
 function pokedexData(data){
 	console.log("Funciona!");
-	var tipoPokemon = data.forms/*[0].name*/;
-	console.log(tipoPokemon+ " esta es la variable");
-	console.log(data+" estos son los datos")
+	var tipoPokemon 	 = data;
+	var name			 = tipoPokemon.forms[0].name;
+	var type1 	 		 = tipoPokemon.types[0].type.name;
+	var type2 	 		 = tipoPokemon.types[1].type.name;
+	var pokeImg			 = tipoPokemon.sprites.front_default;
+
+	$("h2").append(name);
+	$(".type").append(type1+" / "+type2);
+	$("h2").before("<img class='card-img-top' src='" + pokeImg + "'>");
+}
+function pokedexDescription (data){
+	console.log("Funciona tambien!");
+	var descript 		 = data.descriptions[1].description;
+
+	$(".description").append(descript);	
 }
